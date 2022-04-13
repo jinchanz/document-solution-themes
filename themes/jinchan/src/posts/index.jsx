3bf2f813fff20279353a55a679a1c4113a50e711
金禅 <mark.ck@alibaba-inc.com>
2022-04-14 01:54:30 +0800

    chore: refact post detail

--- themes/jinchan/src/posts/index.jsx
+++ themes/jinchan/src/posts/index.jsx
@@ -1,31 +1,28 @@
 import moment from 'moment';
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
+import * as React from 'react';
 import './index.scss';
 
 moment.locale('zh-cn');
-class DocumentView extends Component {
-
-  render() {
-    const { posts } = this.props;
-    return (
-      <main className="home">
-        <ul>
-          { posts.map(item =>
-              <li key={item.title}>
-                <Link to={`/blog/${item.slug}`}>
-                  {item.title}
-                </Link>
-                <time>
-                  {moment(item.first_published_at).fromNow()}
-                </time>
-              </li>
-            ) 
-          }
-        </ul>
-      </main>
-    );
-  }
-}
+const DocumentView = (props) => {
+  const { posts } = props;
+  return (
+    <main className="home">
+      <ul>
+        { posts.map(item =>
+            <li key={item.title}>
+              <Link to={`/blog/${item.slug}`}>
+                {item.title}
+              </Link>
+              <time>
+                {moment(item.first_published_at).fromNow()}
+              </time>
+            </li>,
+          ) 
+        }
+      </ul>
+    </main>
+  );
+};
 
 export default DocumentView;
